2cad8b8b081ad532691376ec3c6612729d7cd932
omar-nif <omarnicanor8@gmail.com>
2025-09-23 20:54:40 -0600

    Organizando codigo

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,54 +1,39 @@
 import express, { Request, Response } from "express";
 import cors from "cors";
 import pino from "pino-http";
-import groupRoutes from "./modules/groups/groups.routes"; // 👈
-import webhookRouter from "./routes/webhook";
+
+//========== Rutas ==================
+import servicesRoutes from './modules/services/services.routes'
+import groupRoutes from "./modules/groups/groups.routes"; 
 import authRoutes from './modules/auth/auth.routes'
+import webhookRouter from "./routes/webhook";
 import paymentRouter from "./routes/payment";
 
 const app = express()
 
-// Rutas de módulos
-//import authRoutes from './modules/auth/auth.routes'
-import servicesRoutes from './modules/services/services.routes'
+// ====== CORS, Body parsers y Logger ======
 app.use(cors({
   origin: ["http://localhost:5173"],
   allowedHeaders: ["Content-Type", "Authorization"],
 }));
-app.use("/webhook", webhookRouter);
-
-// ====== CORS ======
-app.use(
-  cors({
-    origin: ['http://localhost:5173'], 
-    allowedHeaders: ['Content-Type', 'Authorization'],
-  })
-)
-
-// ====== Body parsers ======
+//app.use("/webhook", webhookRouter); // por si falla descomentar
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
-// ====== Logger ======
-// pino-http: log compacto, útil para debug y monitoreo
 app.use(pino())
+
+// ====== RUTAS ESPECIALES ======
+app.use("/webhook", webhookRouter); //stripe consultara directamente (sin /api/)
+
+// ====== Rutas API ======
+app.use('/api/services', servicesRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/groups", groupRoutes); 
+app.use("/api/payment", paymentRouter) 
 
 // ====== Health check ======
 app.get('/', (_req: Request, res: Response) => {
   res.json({ message: 'Connecta+ Backend funcionando' })
 })
 
-// ====== Rutas API v1 ======
-app.use('/api/services', servicesRoutes);
-app.use("/api/auth", authRoutes);
-app.use("/api/groups", groupRoutes); 
-
-// Export para usar en server.ts
-//export default app
-
-
-app.use("/api/payment", paymentRouter) 
-
-// Export para usar en server.ts
 export default app
